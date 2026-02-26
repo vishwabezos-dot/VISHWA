25097c54745040e5f9b99f20d2d277fdcdb73ad3
vishwabezos-dot <vishwabezos@gmail.com>
2026-02-26 10:06:34 +0530

    Create src/App.jsx file with SentenceAI application code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// SentenceAI application code
+import React from 'react';
+
+const SentenceAI = () => {
+    return (
+        <div>
+            <h1>Sentence AI Application</h1>
+            <p>This application analyzes sentences.</p>
+        </div>
+    );
+};
+
+export default SentenceAI;
